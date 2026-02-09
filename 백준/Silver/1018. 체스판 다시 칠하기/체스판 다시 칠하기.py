7834b3599271d1eb55232670269a13ec040d85fa
rlarbfl081513 <arica0101@gmail.com>
2026-02-09 22:54:16 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,51 +1,30 @@
+
 def check_min(a,b,new):
 
-    minmin = float('inf')
+    cnt = 0
+    start = new[a][b]
 
-    for two in ['W','B']:
-        check = [row[:] for row in new]
-        cnt = 0
-        if check[a][b] != two:
-            if check[a][b] == 'W':
-                check[a][b] = 'B'
+    for x in range(a, a + 8):
+        for y in range(b,b+8):
+            # 짝수인데 시작이랑 같은 거면 안바꿔도됨
+            if (x+y)%2 == 0 and new[x][y] != start:
                 cnt += 1
-            else:
-                check[a][b] = 'W'
+            elif (x+y)%2 != 0 and new[x][y] == start:
                 cnt += 1
 
-        for x in range(a,a+8):
-            for y in range(b,b+8):
-                #델타를 통해 오른쪽과 아랫칸 하나씩 확인
-                for q,w in [(0,1),(1,0)]:
-                    nx,ny = x + q, y + w
+    minmin = min(cnt, 64-cnt)
 
-                    # 범위를 넘으면 건너뛰기
-                    if nx < 0 or ny < 0 or nx >= a+8 or ny >= b+8:
-                        continue
-
-                    if check[x][y] == 'W'and check[nx][ny] == 'W':
-                            check[nx][ny] = 'B'
-                            cnt += 1
-                    elif check[x][y] == 'B'and check[nx][ny] == 'B':
-                            check[nx][ny] = 'W'
-                            cnt += 1
-
-        minmin = min(minmin, cnt)
 
     return minmin
-
-
 
 
 n,m = map(int,input().split())
 board = [list(input()) for _ in range(n)]
 min_num = float('inf')
 
-# 8*8의 크기로 볼 시작점 정하기
 for i in range(n-8+1):
     for j in range(m-8+1):
-        new_board = [row[:] for row in board]
-        new_min = check_min(i,j,new_board)
+        new_min = check_min(i,j,board)
         min_num = min(min_num, new_min)
 
 print(min_num)
