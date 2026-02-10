38f1babf0a16a746e161cde8b5306a7cbec837c9
rlarbfl081513 <arica0101@gmail.com>
2026-02-10 09:38:57 +0900

    [Silver V] Title: 영화감독 숌, Time: 1416 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,12 @@
+n = int(input())
+val = 0
+
+# 1부터 주어진 n이 될때까지 1씩더하면서 연속된 6이 들어있는지 확인하기
+for i in range(1,n+1):
+    while True:
+        val += 1
+        # 문자열로 변환하여 연속된 6덩어리가 있는지 확인
+        if "666" in str(val) or "6666" in str(val) or "66666" in str(val):
+            break
+
+print(val)
