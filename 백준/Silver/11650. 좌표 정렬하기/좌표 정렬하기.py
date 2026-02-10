dc4a3a26450c36148c699f40c65bf4a9006e3ef8
rlarbfl081513 <arica0101@gmail.com>
2026-02-10 10:00:52 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 320 ms, Memory: 52648 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -2,8 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = list(list(map(int,input().split(" "))) for _ in range(n))
+arr = []
+
+# 리스트 컴프리헨션보다 append가 가독성이 좋고, 
+# 데이터가 많을 땐 sys.stdin.read().split() 등을 활용하기도 합니다.
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
 arr.sort()
 
+# 출력 최적화: 리스트의 각 원소를 문자열로 바꿔 한 번에 출력
 for i in arr:
-    print(i[0],i[1])
+    sys.stdout.write(f"{i[0]} {i[1]}\n")
