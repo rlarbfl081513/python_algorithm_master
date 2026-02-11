c4914b8eb31e003e05c587d30483902ff5dac1f0
rlarbfl081513 <arica0101@gmail.com>
2026-02-11 09:52:41 +0900

    [Silver V] Title: 숫자 카드, Time: 468 ms, Memory: 121096 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,3 +1,33 @@
+import sys
+
+# 입력 속도 가속
+input = sys.stdin.readline
+
+# 방법 1 : 시간초과
+# n = int(input())
+# n_arr = list(map(int, input().split(" ")))
+#
+# m = int(input())
+# m_arr = list(map(int, input().split(" ")))
+#
+# new = n_arr+m_arr
+#
+# # 중복된 값은 다 지워버리기
+# result = set(i for i in new if new.count(i) == 1)
+#
+# for j in m_arr:
+#     if j in result:
+#         print(0, end=" ")
+#     else:
+#         print(1, end=" ")
+
+
+## 방법 2
+# 시간 복잡도: O(N + M)
+# set 생성: O(N) 
+# m_arr 순회 및 탐색: M * O(1) = O(M)
+# 특징: 파이썬에서 가장 빠르고 직관적인 방법임.
+
 n = int(input())
 n_arr = set(list(map(int, input().split(" "))))
 
@@ -9,3 +39,4 @@ for i in m_arr:
         print(1, end=" ")
     else:
          print(0, end=" ")
+         
