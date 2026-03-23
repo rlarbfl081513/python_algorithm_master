7f9eccfb3894e2edcf7ed73d82dc9559c7aeaecc
rlarbfl081513 <arica0101@gmail.com>
2026-03-23 11:03:12 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,63 +1,49 @@
 
-
 from collections import deque
 
 def find_fr(k):
-    total = 0
-    start = []
+    # 방문 여부와 거리를 동시에 저장할 배열 (-1은 아직 방문 안 함을 의미)
+    distances = [-1] * (n + 1)
+    distances[k] = 0  # 자기 자신(시작점)과의 거리는 0
+    
+    q = deque([k]) # 큐에 시작점만 넣고 출발!
+    
+    while q:
+        current = q.popleft()
+        
+        # 현재 사람과 연결된 모든 친구를 확인
+        for friend in fr[current]:
+            # 아직 방문하지 않은 친구라면 (최단 거리 발견!)
+            if distances[friend] == -1:
+                # 나까지 오는 데 걸린 거리 + 1을 기록
+                distances[friend] = distances[current] + 1
+                q.append(friend)
+                
+    # BFS가 끝나면 distances 배열에는 k에서 모든 사람까지의 최단 거리가 들어있습니다.
+    # 0번 인덱스는 안 쓰니까 [1:]로 슬라이싱해서 1~N번까지의 합을 구합니다.
+    return sum(distances[1:])
 
-    for i in fr[k]:
-        start.append([i,1])
+n, m = map(int, input().split(" "))
+fr = [[] for _ in range(n + 1)]
 
-    for f in range(1,n+1):
-        if f == k:
-            continue
-
-        visit = [0]*(n+1)
-        visit[k] = 1
-
-        q = deque(start)
-
-        while True:
-            spot, lev = q.popleft()
-
-            if visit[spot]:
-                continue
-
-            visit[spot] = 1
-
-            if spot == f:
-                total += lev
-                break
-
-            for j in fr[spot]:
-                q.append([j, lev+1])
-
-    return total
-
-
-n,m  = map(int,input().split(" "))
-fr = [ [] for _ in range(n+1) ]
 for i in range(m):
-    a,b = map(int,input().split(" "))
-    fr[a] = fr[a] + [b]
-    fr[b] = fr[b] + [a]
+    a, b = map(int, input().split(" "))
+    fr[a].append(b) # 리스트 덧셈(+) 대신 append()를 쓰면 속도가 더 빠릅니다!
+    fr[b].append(a)
 
-bacon = [0]
+bacon = [0] # 0번 인덱스 채우기용
 
-for j in range(1,n+1):
+# 1번 사람부터 n번 사람까지 케빈 베이컨의 수를 구함
+for j in range(1, n + 1):
     bacon.append(find_fr(j))
 
-# print(bacon)
+# 작성하셨던 최솟값 및 인덱스 찾기 로직 (아주 좋습니다!)
 minnum = bacon[1]
 index = 1
 
-for a in range(2,n+1):
+for a in range(2, n + 1):
     if minnum > bacon[a]:
         minnum = bacon[a]
         index = a
 
 print(index)
-
-# print(fr)
-# print('total :', bacon)
